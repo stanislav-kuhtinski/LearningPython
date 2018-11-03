8a94f8cad5fe99ac231926a88d519bc510474d83
Stanislav <stanislav.kuhtinski@gmail.com>
2018-11-03 22:04:30 +0200

    Homework2

---
+++ Homework/Home02_Simple.py
@@ -0,0 +1,45 @@
+__author__ = 'stanislav-kuhtinski'
+
+# Task1 - Sorting
+list_to_sort = [4, 3, 8, 1, 0, 7, 5, 9]
+
+sorted1 = sorted(list_to_sort)
+sorted2 = sorted(list_to_sort, reverse=True)
+print('Sorted list 1 = ', sorted1)
+print('Sorted list 2 = ', sorted2)
+
+list_to_sort.sort()  # if you do not need the original list
+print('Sorted list 3 = ', list_to_sort)
+
+# Task2 - tuple with float numbers, find max and min value
+tuple_values = (0.23, 1.16, 0.001, 2.17, 0.02, -3.56, 12.22, 2.13, 3.43, 0.67,)
+max = min = tuple_values[0]
+for item in tuple_values:  # tuple is iterable
+    if item > max:
+        max = item
+    elif item < min:
+        min = item
+print('Max valie is %s, Min value is %s' % (max, min))
+
+# Task 3 - list to string
+address = ['Earth', 'Estonia', 'Tallinn', 'Kivila4']
+address_string = ''
+for part in address:
+    if not '':
+        address_string += part
+    if part != address[-1]:
+        address_string += ' -> '
+print("\nPosting address is ", address_string)
+
+# Task 3 - separate string into list by symbol
+path = '/bin:/usr/bin:/usr/local/bin'
+path_parts = path.split(':')
+print('List of path_parts', path_parts)
+print('Full path is: ', path_parts[-1])
+
+# Task 4 - all numbers in range(1-100) divided by 7
+list_of_outputs = [] #empty list
+for i in range(100):
+    if i % 7 == 0:
+        list_of_outputs.append(i)
+print('Values in range 100 divided by 7: ', list_of_outputs)
