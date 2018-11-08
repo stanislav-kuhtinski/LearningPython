e6344bebf3dc7d86b5ae96c4ece15d7efe0641da
Stanislav <stanislav.kuhtinski@gmail.com>
2018-11-08 18:04:25 +0200

    Adding Homework2

--- Homework/Home02_Simple.py
+++ Homework/Home02_Simple.py
@@ -13,17 +13,20 @@ print('Sorted list 3 = ', list_to_sort)
 
 # Task2 - tuple with float numbers, find max and min value
 tuple_values = (0.23, 1.16, 0.001, 2.17, 0.02, -3.56, 12.22, 2.13, 3.43, 0.67,)
-max = min = tuple_values[0]
+# print('Max value is ', max(tuple_values))
+# print('Min value is ', min(tuple_values))
+max_v = min_v = tuple_values[0]
 for item in tuple_values:  # tuple is iterable
-    if item > max:
-        max = item
-    elif item < min:
-        min = item
-print('Max valie is %s, Min value is %s' % (max, min))
+    if item > max_v:
+        max_v = item
+    elif item < min_v:
+        min_v = item
+print('Max valie is %s, Min value is %s' % (max_v, min_v))
 
 # Task 3 - list to string
 address = ['Earth', 'Estonia', 'Tallinn', 'Kivila4']
 address_string = ''
+# address_string = str(address[0] + ' -> ' + address[1] + ' -> ' + address[2])
 for part in address:
     if not '':
         address_string += part
@@ -38,8 +41,16 @@ print('List of path_parts', path_parts)
 print('Full path is: ', path_parts[-1])
 
 # Task 4 - all numbers in range(1-100) divided by 7
-list_of_outputs = [] #empty list
+list_of_outputs = []  # empty list
 for i in range(100):
     if i % 7 == 0:
         list_of_outputs.append(i)
-print('Values in range 100 divided by 7: ', list_of_outputs)
+print('\nValues in range 100 divided by 7: ', list_of_outputs)
+
+# Task 5 - delete some value in list
+some_data = ['to-delete', '1', 'to-delete', '2', '3', '4', 'to-delete', '5']
+print('\nOriginal list: ', some_data)
+# some_data = [data for data in some_data if data != 'to-delete']
+while 'to-delete' in some_data:
+    some_data.remove('to-delete')
+print('Corrected list: ', some_data)
