2bf6e116504fb378a16cd553191c8515ce6543a6
Stanislav <stanislav.kuhtinski@gmail.com>
2019-02-11 16:14:10 +0200

    Add Homework 7 - Decorators and Syntatic Sugar

--- Homework/Home07_Decorators.py
+++ Homework/Home07_Decorators.py
@@ -3,27 +3,53 @@ __author__ = 'stanislav-kuhtinski'
 from datetime import datetime
 
 
+# --------------------------------------------------
+# Task3. Count the function execution times
+def count_executions(func):
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        # print('result of func() ', func(*args, **kwargs))
+        return func
+
+    wrapper.calls = 0
+    return wrapper
+
+
+class MyList(list):
+    @count_executions
+    def sum_two_numbers(self, a, b):
+        return (self, a + b)
+
+
+pairs = MyList([1, 2, 3, 4, 5])
+for i in range(len(pairs) - 1):
+    pairs.sum_two_numbers(pairs[i], pairs[i + 1])
+print('Fuction has been executed {} times'.format(pairs.sum_two_numbers.calls))
+
+
+# --------------------------------------------------
 # Task2. Measures the speed of functions performance
 def measure_time(slow_funk):
     def wrapper(*args, **kwargs):
         start_time = datetime.now()
         try:
             slow_funk(*args, **kwargs)
-        except TypeError as e:
+        except Exception as e:
             print('Type Error', e)
-        print(datetime.now() - start_time)
+        print(slow_funk.__name__, ' executed in ', datetime.now() - start_time)
+
     return wrapper
 
 
 @measure_time  # decorate for speed measurment
-def slow_function(n=10000):
+def slow_function(n=100000):
     total = 0
     for i in range(n):
         total += i
-        print('{} total {}'.format(i, total))
     return total
 
 
+# --------------------------------------------------
 # Task1. Cancels execution of any function
 def cansel_function(func):
     def inner(*args, **kwargs):
@@ -40,5 +66,5 @@ def say_hello(name="everybody"):
 
 if __name__ == '__main__':
     # Basic functions:
-    # say_hello()
+    say_hello()
     slow_function()
