9104e7c9dfb6252001a51c9fe8ad1769d6d48f08
Stanislav <stanislav.kuhtinski@gmail.com>
2019-02-24 13:07:23 +0200

    Refactor Homework 9 - Reading files with regex

--- Homework/Home09_Reading_Files.py
+++ Homework/Home09_Reading_Files.py
@@ -4,17 +4,15 @@ import re
 import json
 
 
-def write_to_file(filename, content, mode='w'):
-    with open(filename, mode=mode) as filehandle:
-        filehandle.write(json.dumps(content, indent=4))
-    print('Data has been sucessfully save to', filename)
-
 def read_file(file_to_open):
     try:
         my_file = open(file_to_open)
     except Exception as error:
         print("File not found or path is incorrect ", error)
+    return my_file
 
+
+def parce_file(my_file):
     regex_for_timestamp = r'\d{2}/\d{2}.\d{2}:\d{2}:\d{2}'
     regex_for_status = r'[A-Z]+\s+:|[A-Z]+:{1}'
     regex_for_action = r'[a-z]+_[a-z]+'
@@ -23,7 +21,6 @@ def read_file(file_to_open):
     regex_del_from_txt = r':{1}\s'
     content_list = []
 
-    # Get next line from file
     while True:
         # Get next line from file
         line = my_file.readline()
@@ -38,7 +35,7 @@ def read_file(file_to_open):
                + [re.sub(regex_del_from_txt, '', i) for i in log_info]
         content_dict = add_header(data)
         content_list.append(content_dict)  # append dictionary to list
-    print('File {} has been sucessfully read' .format(file_to_open))
+    print('File {} has been sucessfully read'.format(my_file.name))
     return content_list
 
 
@@ -50,9 +47,16 @@ def add_header(my_list):
     return my_dict
 
 
+def write_to_file(filename, content, mode='w'):
+    with open(filename, mode=mode) as filehandle:
+        filehandle.write(json.dumps(content, indent=4))
+    print('Data has been sucessfully save to', filename)
+
+
 data_folder = Path('Files')
 file_to_read = data_folder / "example_log.log"
 file_to_save = data_folder / "example_log_edited.log"
 content_raw_list = read_file(file_to_read)
+content_raw_list = parce_file(content_raw_list)
 # print(json.dumps(content_raw_list, indent=4))  # indent - number of spaces to indent by
 write_to_file(file_to_save, content_raw_list)
